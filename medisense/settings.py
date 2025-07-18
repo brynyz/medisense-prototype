6163c222044220ac0fd9179f3c0a6fd256e90440
Brylle Nyel <bryllenyelm@gmail.com>
2025-07-18 10:44:10 +0800

    Fixed Settings Syntax Error

--- medisense/settings.py
+++ medisense/settings.py
@@ -183,14 +183,14 @@ CAPTCHA_NOISE_FUNCTIONS = (
     # 'captcha.helpers.noise_null',
 )
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bryllenyelm@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'bryllenyelm@gmail.com'
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 # Security settings for production
